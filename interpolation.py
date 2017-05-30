b8e5fa3e3b0a73cfd057968effc9cf65af45d8c3
Pedro Braga <pbragaalves@github.com>
2017-05-30 19:05:02 -0300

    Final result

--- interpolation.py
+++ interpolation.py
@@ -1,6 +1,4 @@
-import scipy.interpolate as si
 import matplotlib.pyplot as plt
-from numpy.polynomial.hermite import hermfit, hermval
 import numpy as np
 LOD = 15;
 
@@ -18,32 +16,32 @@ def calc_hermite(x1, x2, y1, y2, t1, t2):
 	return xnew, ynew
 
 def plot_hermite(x, y):
-	t1 = float(x[1]-x[0])/float(2), float(y[1]-y[0])/float(2)
-	t2 = float(x[2]-x[0])/float(2), float(y[2]-y[0])/float(2) 
+	t1 = (x[1]-x[0])/2, (y[1]-y[0])/2
+	t2 = (x[2]-x[0])/2, (y[2]-y[0])/2 
 	xnew, ynew = calc_hermite(x[0], x[1], y[0], y[1], t1, t2)
-	for j in range(1, len(x)-3):
+	for j in range(1, len(x)-2):
 		x1, x2, x3, x4 = x[j-1], x[j], x[j+1], x[j+2]
 		y1, y2, y3, y4 = y[j-1], y[j], y[j+1], y[j+2]
-		t1 = float(x3-x1)/float(2), float(y3-y1)/float(2)
-		t2 = float(x4-x2)/float(2), float(y4-y2)/float(2)
+		t1 = (x3-x1)/2, (y3-y1)/2
+		t2 = (x4-x2)/2, (y4-y2)/2
 		auxx, auxy = calc_hermite(x2, x3, y2, y3, t1, t2)
 		for i in auxx: 
 			xnew.append(i)
 		for i in auxy:
 			ynew.append(i)
 	l = len(x)-1
-	t1 = float(x[l]-x[l-2])/float(2), float(y[l]-y[l-2])/float(2)
-	t2 = float(x[l]-x[l-1])/float(2), float(y[l]-y[l-1])/float(2)
+	t1 = (x[l]-x[l-2])/2, (y[l]-y[l-2])/2
+	t2 = (x[l]-x[l-1])/2, (y[l]-y[l-1])/2
 	auxx, auxy = calc_hermite(x[l-1], x[l], y[l-1], y[l], t1, t2)
 	for i in auxx: 
 		xnew.append(i)
 	for i in auxy:
 		ynew.append(i)	
 
-	plt.plot(x, y, 'kx', xnew, ynew, 'b-')
+	plt.plot(xnew, ynew, '-')
 	plt.show()
 
-with open("./pontos2.txt", 'r') as lines:
+with open("./pontos3.txt", 'r') as lines:
 	points = [i.strip().split(",") for i in lines]	
 	x = [float(i[0]) for i in points]
 	y = [-1*float(i[1]) for i in points]
